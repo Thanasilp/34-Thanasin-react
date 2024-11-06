e89fa5fa13f1a76c0d38b5350e769a0d6ebaea5b
unknown <thanasin.pin@gmail.com>
2024-11-06 18:48:37 +0700

    modified ShowingCustomer.tsx to hide the table when there is no data showing

--- src/components/ShowingCustomer.tsx
+++ src/components/ShowingCustomer.tsx
@@ -22,24 +22,28 @@ export const ShowingCustomer = () => {
   console.log(customers);
   return (
     <section className="flex justify-center mx-auto mt-4">
-      <table className="w-full max-w-md border border-gray-300">
-        <thead>
-          <tr className="bg-slate-200">
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Career</th>
-          </tr>
-        </thead>
-        <tbody>
-          {customers.map((customer) => (
-            <tr key={customer.id}>
-              <td>{customer.firstName}</td>
-              <td>{customer.lastName}</td>
-              <td>{customer.career}</td>
+      {customers.length > 0 ? (
+        <table className="w-full max-w-md border border-gray-300">
+          <thead>
+            <tr className="bg-slate-200">
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Career</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {customers.map((customer) => (
+              <tr key={customer.id}>
+                <td>{customer.firstName}</td>
+                <td>{customer.lastName}</td>
+                <td>{customer.career}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p className="text-center text-gray-500">No data available</p>
+      )}
     </section>
   );
 };
